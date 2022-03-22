de6daaa6c619d9b683cc5f9af5e8b32100935fa7
louisTUS <louis.charentus@gmail.com>
2022-03-22 15:02:20 +0100

    Erreur global dans mine et tour

---
+++ TownHall.java
@@ -0,0 +1,13 @@
+public class TownHall extends Batiment {
+    
+    public Batiment(int cout, Joueur appartient, int income){
+
+    super(cout, appartient, income, "TownHall");
+    
+
+  }
+
+
+
+
+}

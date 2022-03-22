e015cfa15f8d847b19a0c6fe9961765a9e568fe7
louisTUS <louis.charentus@gmail.com>
2022-03-22 14:39:18 +0100

    Classe Tour

---
+++ Tour.java
@@ -0,0 +1,14 @@
+public class Tour extends Batiment {
+    private String type;
+    
+    public Batiment(int cout, Joueur appartient){
+
+    super(cout, appartient, 0, "Tour");
+    
+
+  }
+
+
+
+
+}

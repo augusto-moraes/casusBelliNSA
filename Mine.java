b95fe8a0bb01a1b43dec0127fac50afa2e4c5706
louisTUS <louis.charentus@gmail.com>
2022-03-22 14:36:04 +0100

    Point virgule manquant

--- Mine.java
+++ Mine.java
@@ -3,7 +3,7 @@ public class Mine extends Batiment {
     
     public Batiment(int cout, Joueur appartient, int income){
 
-    super(cout, appartient, income, "Mine")
+    super(cout, appartient, income, "Mine");
     
 
   }

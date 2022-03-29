4c225bf8227ed0ce09dc7618b8b8507958347653
louisTUS <louis.charentus@gmail.com>
2022-03-29 14:25:14 +0200

    joueur upgrade

--- Joueur.java
+++ Joueur.java
@@ -1,13 +1,28 @@
 public class Joueur{
+    
     private String nom;
     private static int nbJoueur= 0;
-    
     private int id;
+    private String color;
     private boolean isAlive;
     private boolean bot;
-    public Joueur(String nom) {
+    
+    public Joueur(String nom, String color) {
         nbJoueur ++;
         id=nbJoueur;
+        this.nom=nom;
+        this.color=color;
     }
-
+    
+    public String getcolor(){
+        return color;
+    }
+    
+    public int getid(){
+        return id;
+    }
+    
+    public String getnom(){
+        return nom;
+    }
 }
